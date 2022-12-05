bb483779918a4b36f996ea7ae3ca4f2ccb7013a8
AKsondzyk <andrksondzik@gmail.com>
2022-12-05 16:45:16 +0200

    05.12.2022_16:45

---
+++ JS/01_lection/JS/numberOperation.js
@@ -0,0 +1,19 @@
+let NuberOne = parseInt(prompt('Введіть перше число для математичних операцій'));
+let UserFirstNumber = 'Ваше перше введене число = ' + NuberOne;
+
+let NuberTwo = parseInt(prompt('Введіть друге число для математичних операцій' ));
+let UserSecondtNumber = 'Ваше друге введене введене число = ' + NuberTwo;
+
+console.log(UserFirstNumber,);
+console.log(UserSecondtNumber);
+
+let SumOperation = NuberOne + NuberTwo;
+let DiffernsOperation = NuberOne - NuberTwo;
+let MultiplicationOperation = NuberOne * NuberTwo;
+let DivisionOperation = NuberOne / NuberTwo;
+
+console.log('Сума двох чисел',SumOperation)
+console.log('Різниця двох чисел',DiffernsOperation)
+console.log('Множення двох чисел',MultiplicationOperation)
+console.log('Ділення першого на друге',DivisionOperation)
+
---
+++ JS/01_lection/JS/textOperation.js
@@ -0,0 +1,5 @@
+let FirsName = prompt('Введіть Ваше ім`я');
+let SecondName = prompt('Введіть як Вас по батькові' );
+
+document.write('Вітаю ' + FirsName + ' ' + SecondName + ' бажаю гарного дня!')
+
