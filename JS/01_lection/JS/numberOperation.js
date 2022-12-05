b856a4522201a22078379156abcd1c800830f4cc
AKsondzyk <andrksondzik@gmail.com>
2022-12-05 18:00:58 +0200

    05.12.2022_18:00

--- JS/01_lection/JS/numberOperation.js
+++ JS/01_lection/JS/numberOperation.js
@@ -1,7 +1,7 @@
-let NuberOne = parseInt(prompt('Введіть перше число для математичних операцій'));
+let NuberOne = parseFloat(prompt('Введіть перше число для математичних операцій'));
 let UserFirstNumber = 'Ваше перше введене число = ' + NuberOne;
 
-let NuberTwo = parseInt(prompt('Введіть друге число для математичних операцій' ));
+let NuberTwo = parseFloat(prompt('Введіть друге число для математичних операцій' ));
 let UserSecondtNumber = 'Ваше друге введене введене число = ' + NuberTwo;
 
 console.log(UserFirstNumber,);
@@ -17,3 +17,9 @@ console.log('Різниця двох чисел',DiffernsOperation)
 console.log('Множення двох чисел',MultiplicationOperation)
 console.log('Ділення першого на друге',DivisionOperation)
 
+// --------------------------------------------------------
+
+let FirstNuberMoreThanTwenty = parseFloat(prompt('Введіть перше число що більше 20' ));
+let SecondNuberMoreThanThirty = parseFloat(prompt('Введіть перше число що більше 30' ));
+
+console.log('Чи введено перше число > 20 а друге > 30 ?', FirstNuberMoreThanTwenty>20 && SecondNuberMoreThanThirty>30);
--- JS/01_lection/JS/textOperation.js
+++ JS/01_lection/JS/textOperation.js
@@ -1,5 +1,11 @@
 let FirsName = prompt('Введіть Ваше ім`я');
 let SecondName = prompt('Введіть як Вас по батькові' );
 
-document.write('Вітаю ' + FirsName + ' ' + SecondName + ' бажаю гарного дня!')
+document.write('User name: ' + FirsName + '<br>' + ' User last name: ' + SecondName);
 
+// ----------------------------------------------------------
+
+let LANG = (prompt('Введіть бажану мову "uk" або "ru"').toLowerCase());
+let RightLang = uk;
+
+console.log('Чи вибрано мову uk?', LANG === RightLang);
