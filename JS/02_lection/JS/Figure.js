4814b51cca36ea944eed2d03ad8170e57456bd5a
AKsondzyk <andrksondzik@gmail.com>
2022-12-15 18:21:52 +0200

    15_12_2022_18:21

--- JS/02_lection/JS/Figure.js
+++ JS/02_lection/JS/Figure.js
@@ -3,14 +3,24 @@
 // ------------------------------------------------------------
 let FigureWidht = parseInt(prompt('введіть ширину фігури',1));
 let FigureHeight = parseInt(prompt('введіть висоту фігури',1));
-let FigureROW = ' ';
-let FigureColumn = ' ';
+let FigureROW = '';
+let  FigureColumn = '';
 
 for (x = 0; x < FigureWidht; x++) {
-    FigureROW = FigureROW + '*';
-    console.log(FigureROW);
-}
-for (y = 0; y < FigureHeight; y++) {
-    FigureColumn = FigureColumn + '*';
+   FigureROW = FigureROW + '*';
+   for (y = 0; y < FigureHeight; y++) {
+    FigureColumn += FigureROW ;
+//     FigureColumn = FigureColumn + '*';
+    // console.log(FigureColumn);
     console.log(FigureColumn);
 }
+}
+// console.log(FigureROW);
+// ------------------
+// for (y = 0; y < FigureHeight; y++) {
+    // FigureColumn = FigureROW;
+//     FigureColumn = FigureColumn + '*';
+    // console.log(FigureColumn);
+//     console.log(FigureROW)
+
+// }
