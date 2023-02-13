afe51e29fe218af6437226ece13a2354c101a8c8
AKsondzyk <andrksondzik@gmail.com>
2023-02-13 13:14:41 +0200

    13.02.2023_13:14

--- JS/12_lection/JS/Dz12.js
+++ JS/12_lection/JS/Dz12.js
@@ -5,43 +5,121 @@
 let body = document.body;
 
 let Form = document.createElement('form');
-let Input = document.createElement('input');
-Input.setAttribute('type', 'checkbox');
-let Label = document.createElement('label');
-Label.textContent = 'Еліпс';
-let button = document.createElement('button');
-button.textContent = 'Намалювати Еліпс';
-button.disabled = true;
-
 let FormWidth = prompt(`Яка довжина форми завдання 1?`, 150);
 let FormHeight = prompt(`Яка висота форми завдання 1?`, 30);
-
 Form.setAttribute('style', `width: ${FormWidth}px; height:${FormHeight}px;`);
+Form.addEventListener('submit', function (e) {
+    e.preventDefault()
+});
 
+let Input = document.createElement('input');
+Input.setAttribute('type', 'checkbox');
+
+let flag = false;
+Input.onchange = function () {
+    flag = Input.checked;
+    console.log(`flag = ${flag}`);
+};
+
+let Label = document.createElement('label');
+Label.textContent = 'Еліпс';
+
+let button = document.createElement('button');
+button.textContent = 'Намалювати Еліпс';
+
+let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+svg.setAttribute('style', 'width: 500px; height: 200px; margin-top: 20px;');
+
+// ------Додавання елементів в HTML-----
 Form.appendChild(Input);
 Form.appendChild(Label);
 Form.appendChild(button);
 
 body.appendChild(Form);
+body.appendChild(svg);
 
+// -----Малювання------------------------
+button.addEventListener('click', function (e) {
+    if (flag) {
+        let ellipse = document.createElementNS('http://www.w3.org/2000/svg', 'ellipse');
+        ellipse.setAttribute('cx', '300');
+        ellipse.setAttribute('cy', '100');
+        ellipse.setAttribute('rx', '100');
+        ellipse.setAttribute('ry', '50');
+        ellipse.setAttribute('style', 'fill: yellow; stroke: red;');
 
+        // svg.removeChild(rect);
+        svg.appendChild(ellipse);
+    }
+    else {
+        let rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+        rect.setAttribute('width', '300');
+        rect.setAttribute('height', '100');
+        rect.setAttribute('style', 'fill: black; stroke: red;');
 
-Label.addEventListener('check', function(){
-    console.log(Label);
-    button.disabled = false;
-    return;
+        // svg.removeChild(ellipse);
+        svg.appendChild(rect);
+    }
 });
-
 
 
 // Cтворити інпут куди водиться значення і кнопку . Після введення значення ( цифрового ) і натискання на кнопку
 // малються кола за допомогою svg. При натисканні на намальованне коло воно повино видалятись.
 
+let Form2 = document.createElement('form');
+Form2.setAttribute('style', `width: 3000px; height:50px; margin: 50px auto;`);
+Form2.addEventListener('submit', function (e) {
+    e.preventDefault()
+});
 
+let Input2 = document.createElement('input');
+Input2.setAttribute('type', 'number');
+let Radius = 0;
 
+// console.log(Input2);
+// console.log(Radius);
 
+let Label2 = document.createElement('label');
+Label2.textContent = 'Введіть радіус кола';
 
-// Створити функцію яка малює коло в канвасі і розміщує в середині кола текст 
+let button2 = document.createElement('button');
+button2.textContent = 'Намалювати коло';
+
+let svg2 = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+svg2.setAttribute('style', 'width: 500px; height: 200px; margin-top: 20px;');
+
+// ------Додавання елементів в HTML-----
+Form2.appendChild(Label2);
+Form2.appendChild(Input2);
+Form2.appendChild(button2);
+
+body.appendChild(Form2);
+body.appendChild(svg2);
+
+// ---Малювання---
+
+let circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+
+button2.addEventListener('click', function (e) {
+    Radius = Input2.value;
+    // console.log(Radius);
+
+    circle.setAttribute('style', 'fill: red; stroke: blue;');
+    circle.setAttribute('cx', '300');
+    circle.setAttribute('cy', '100');
+    circle.setAttribute('r', `${Radius}`);
+
+    svg2.appendChild(circle);
+    return;
+});
+
+// ---Видаляння кола при натисканні------------
+
+svg2.addEventListener('click', function (e) {
+    svg2.removeChild(circle);
+});
+
+// Створити функцію яка малює коло в канвасі і розміщує в середині кола текст
 
 
 // ***Додадткове завдання, створити функцію яка отримує массив данних, колір. І виводить графік на канвасі
