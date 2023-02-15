cfcddd98ec4f2bbc1731d2ab2900f0290d2bc98d
AKsondzyk <andrksondzik@gmail.com>
2023-02-15 12:27:22 +0200

    15.02.2023_12:27

--- JS/13_lection/JS/DZ13.js
+++ JS/13_lection/JS/DZ13.js
@@ -22,62 +22,58 @@ xhr.send();
 // 2)При кліку на кнопку "select" вибрані іd користувачів повині записувати в  localStorage,  
 // також повинно бути записано в localStorage скільки користувачів вибрано.
 
+let i=0;
+
 let TaskOne = function () {
     result.forEach((item) => {
     // ---------створення елементів------
         let CreateDiv = document.createElement('div');
-        //     card.classList.add('card');
+        CreateDiv.setAttribute('style', 'display: flex; justify-content: space-around; align-items: center; border: 2px solid black;');
 
         let ul = document.createElement('ul');
+        ul.setAttribute('style', 'list-style-type: none;');
 
         let li = document.createElement('li');
-        //     li.classList.add('item-list');
         li.innerText = `id: ${item.id}`;
 
         let name = document.createElement('li');
-        //     name.classList.add('item-list', 'search');
+        name.setAttribute('class', 'name');
         name.innerText = item.name;
 
         let username = document.createElement('li');
-        //     username.classList.add('item-list');
         username.innerText = `Username:  ${item.username}`;
 
         let address = document.createElement('li');
+        address.innerHTML = 'Address';
         let addressUl = document.createElement('ul');
-        //     address.classList.add('item-list');
-        addressUl.innerHTML = `Address:<li>City: ${item.address.city}</li>
-                                   <li>GEO: lat ${item.address.geo.lat} lng ${item.address.geo.lng}</li>
-                                   <li>Street: ${item.address.street}</li>
-                                   <li>Suite: ${item.address.suite}</li>
-                                   <li>ZIP: ${item.address.zipcode}</li>`;
+        addressUl.setAttribute('style', 'list-style-type: none;');
+        addressUl.innerHTML = ` <li>City: ${item.address.city}</li>
+                                <li>GEO: lat ${item.address.geo.lat} lng ${item.address.geo.lng}</li>
+                                <li>Street: ${item.address.street}</li>
+                                <li>Suite: ${item.address.suite}</li>
+                                <li>ZIP: ${item.address.zipcode}</li>`;
 
         let company = document.createElement('li');
+        company.innerHTML = 'Company';
         let companyUL  = document.createElement('ul');
-        //     company.classList.add('item-list');
-        companyUL.innerHTML = `Company:<li>Name: ${item.company.name}</li>
-                                    <li>Bs: ${item.company.bs}</li>
-                                    <li>Catch Phrase: ${item.company.catchPhrase}</li>`;
+        companyUL.setAttribute('style', 'list-style-type: none;');
+        companyUL.innerHTML = ` <li>Name: ${item.company.name}</li>
+                                <li>Bs: ${item.company.bs}</li>
+                                <li>Catch Phrase: ${item.company.catchPhrase}</li>`;
 
 
         let email = document.createElement('li');
-        //     email.classList.add('item-list');
         email.innerText = ` Email: ${item.email}`;
 
         let phone = document.createElement('li');
-        //     phone.classList.add('item-list');
         phone.innerText = ` Phone: ${item.phone}`;
 
         let website = document.createElement('li');
-        //     website.classList.add('item-list');
         website.innerText = ` Website: ${item.website}`;
 
-
-
         let ButtonDiv = document.createElement('div');
-        //     btnBox.classList.add('btn-box');
 
         let button = document.createElement('button');
-        //     btn.classList.add('btn');
         button.innerText = 'Select';
 
     // ---------додавання елементів в документ------
@@ -103,12 +99,12 @@ let TaskOne = function () {
 
         body.appendChild(CreateDiv);
 
-    //     button.onclick = () => {
-    //         localStorage.setItem('id', `${item.id}`);
-    //         localStorage.counterKey = ++counter
-    //         localStorage[`${item.name}`] = `ID ${item.id}`;
+        // ---подія на клік----------
 
-     //     }
+        button.onclick = () => {
+            localStorage.counterKey = ++i;
+            localStorage.setItem('id', `${item.id}`);
+         }
     });
 };
 
@@ -117,3 +113,26 @@ xhr.onload = TaskOne;
 // Додадткове завдання:
 // Створити інпут в якому можна буде шукати користувачів по імені. (всі користувачі в яких не співпадає імя 
 // повині зникати);
+
+let SearchLabel = document.createElement('label');
+SearchLabel.setAttribute('for', 'Search');
+SearchLabel.innerText = 'Знайти по імені';
+
+let SearchInput = document.createElement('input');
+SearchLabel.setAttribute('type', 'search');
+SearchLabel.setAttribute('id', 'Search');
+
+let ButtonSearch = document.createElement('button');
+ButtonSearch.innerText = 'Шукати';
+
+body.appendChild(SearchLabel);
+body.appendChild(SearchInput);
+body.appendChild(ButtonSearch);
+
+let AllName = document.querySelectorAll('.name');
+ArrName = Array.from(AllName);
+
+console.log(ArrName); // не добавляються елементи в масив
+
+
+
