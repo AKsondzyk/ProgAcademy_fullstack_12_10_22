4b7af736a9ab3b3308c9f18c48a0ff7044b82039
AKsondzyk <andrksondzik@gmail.com>
2023-02-20 20:24:57 +0200

    20.02.2023_20:24

---
+++ JS/15_lection/Gulp/Ready_Data/JS/JS/Dz15.js
@@ -0,0 +1,12 @@
+let body = document.querySelector('body');
+
+let p = document.querySelector('p');
+p.setAttribute('style', 'color: red; font-size: 2rem;');
+
+console.log(p);
+
+let div = document.createElement('div');
+div.textContent = 'New Text from JS';
+div.setAttribute('style', 'bacground-color: orange;');
+
+body.appendChild(div);
